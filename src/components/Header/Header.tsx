brunurd/bruno-conway-web-app

fix: eslint config

src/components/Header/Header.tsx
@@ -31,8 +31,8 @@ const Header = () => {
       />
       {showing && (
         <>
-          <Title>Bruno Araujo's </Title>
-          <Subtitle>Conway's Game of Life</Subtitle>
+          <Title>{'Bruno Araujo\'s'}</Title>
+          <Subtitle>{'Conway\'s Game of Life'}</Subtitle>
           <Spacing margin="20px 0 0">
             <Row>
               <Button

leonaaraujo/leona-conway-web-app

src/components/Header/Header.tsx
@@ -31,8 +31,8 @@ const Header = () => {
       />
       {showing && (
         <>
-          <Title>Leona Araujo's </Title>
-          <Subtitle>Conway's Game of Life</Subtitle>
+          <Title>{'Leona Araujo\'s'}</Title>
+          <Subtitle>{'Conway\'s Game of Life'}</Subtitle>
           <Spacing margin="20px 0 0">
             <Row>
               <Button