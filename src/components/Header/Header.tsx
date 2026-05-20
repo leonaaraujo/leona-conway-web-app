brunurd/bruno-conway-web-app

feat: advance states X times button

src/components/Header/Header.tsx
@@ -1,0 +1,63 @@
+import { useContext, useState } from 'react';
+import {
+  Button,
+  Title,
+  Subtitle,
+  Row,
+  Spacing,
+} from '../../ui/components';
+import { BoardContext } from '../../contexts/BoardContext';
+import {
+  FloatHeader,
+  HideMenuIcon,
+} from './Header.styles';
+import { AdvanceStatesForm } from '../AdvanceStatesForm';
+
+const Header = () => {
+  const {
+    runSimulation,
+    reset,
+    nextState,
+    data: { running },
+  } = useContext(BoardContext);
+  const [showing, setShowing] = useState(true);
+
+  return (
+    <FloatHeader $showing={showing}>
+      <HideMenuIcon
+        $showing={showing}
+        onClick={() => setShowing(!showing)}
+      />
+      {showing && (
+        <>
+          <Title>Bruno Araujo's </Title>
+          <Subtitle>Conway's Game of Life</Subtitle>
+          <Spacing margin="20px 0 0">
+            <Row>
+              <Button
+                disabled={running}
+                onClick={() => nextState()}
+              >
+                Next State
+              </Button>
+              <Button
+                disabled={running}
+                onClick={() => runSimulation()}
+              >
+                Run Simulation
+              </Button>
+              <Button
+                onClick={() => reset()}
+              >
+                Reset
+              </Button>
+            </Row>
+          </Spacing>
+          <AdvanceStatesForm />
+        </>
+      )}
+    </FloatHeader>
+  );
+};
+
+export { Header };

leonaaraujo/leona-conway-web-app

src/components/Header/Header.tsx
@@ -1,0 +1,63 @@
+import { useContext, useState } from 'react';
+import {
+  Button,
+  Title,
+  Subtitle,
+  Row,
+  Spacing,
+} from '../../ui/components';
+import { BoardContext } from '../../contexts/BoardContext';
+import {
+  FloatHeader,
+  HideMenuIcon,
+} from './Header.styles';
+import { AdvanceStatesForm } from '../AdvanceStatesForm';
+
+const Header = () => {
+  const {
+    runSimulation,
+    reset,
+    nextState,
+    data: { running },
+  } = useContext(BoardContext);
+  const [showing, setShowing] = useState(true);
+
+  return (
+    <FloatHeader $showing={showing}>
+      <HideMenuIcon
+        $showing={showing}
+        onClick={() => setShowing(!showing)}
+      />
+      {showing && (
+        <>
+          <Title>Leona Araujo's </Title>
+          <Subtitle>Conway's Game of Life</Subtitle>
+          <Spacing margin="20px 0 0">
+            <Row>
+              <Button
+                disabled={running}
+                onClick={() => nextState()}
+              >
+                Next State
+              </Button>
+              <Button
+                disabled={running}
+                onClick={() => runSimulation()}
+              >
+                Run Simulation
+              </Button>
+              <Button
+                onClick={() => reset()}
+              >
+                Reset
+              </Button>
+            </Row>
+          </Spacing>
+          <AdvanceStatesForm />
+        </>
+      )}
+    </FloatHeader>
+  );
+};
+
+export { Header };